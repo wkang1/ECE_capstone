22ec438897fcb41e9a78aee67120dba6beee6b81
Jeremy Ryu <jeremyryuacea@gmail.com>
2022-03-16 11:55:52 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,50 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
+import { StyleSheet } from 'react-native';
+import NewScreen from './src/screens/NewScreen';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import Toast from 'react-native-toast-message';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+    screenOptions={{
+			headerStyle: {
+				backgroundColor: '#228CDB'
+			},
+        	headerTintColor: '#fff'
+    	}} 
+    initialRouteName="Home">
+    <Stack.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={({navigation}) => ({
+            headerRight: () => (
+                <Icon 
+                name="plus" 
+                type="feather" 
+                color="#fff"
+                style={style.headerIcon}
+                                    onPress={() => navigation.navigate('New')}
+            />
+          )
+        })}
+    />
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
+    </NavigationContainer>
+  );
+}
+
+const style = StyleSheet.create({
+  headerIcon: {
+    marginRight: 10
+  }
+});
