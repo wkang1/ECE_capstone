e0d1363c80ea7e709399a9f2a1e2f0b7e325a472
Wonho Kang <wkang1@andrew.cmu.edu>
2022-04-11 11:43:09 -0400

    cropping image script

---
+++ crop_image/cropping.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import PIL
+import json
+
+img = Image.open('/home/d4/arducam_images/019_rpi_v3_imx477_cam0.jpeg')
+file = open('/home/d4/openpose_json/019_rpi_v3_imx477_cam0_keypoints.json')
+
+data = json.load(file)
+
+left_crop = data['part_candidates']['3'][0]
+right_crop = data['part_candidates']['6'][0]
+top_crop = data['part_candidates']['1'][1]
+bottom_crop = data['part_candidates']['8'][1]
+
+#crop according to dimensions
+cropped_img = img.crop((left_crop, top_crop, right_crop, bottom_crop))
+
+# save image
+cropped_img.save("cropped_image.jpg")
