190a974bd7a57401e34fdfb5abce163b82badadc
Jeremy Ryu <jeremyryuacea@gmail.com>
2022-04-19 18:38:37 -0400

    able to upload image display urls to spreadsheet

--- AddPhotoScreen.js
+++ AddPhotoScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import axios from "axios";
 import { View, Image, Button, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 const SERVER_URL = 'https://sheet.best/api/sheets/725a3d83-79cc-4a91-90ec-aefe53604c53';
+const LOCALHOST_URL = 'http://10.0.0.84:3000';
 
-const createFormData = (photo, body = {}) => {
+const createFormData = (photo) => {
   const data = new FormData();
 
   data.append('photo', {
@@ -14,14 +16,12 @@ const createFormData = (photo, body = {}) => {
     uri: Platform.OS === 'ios' ? photo.uri.replace('file://', '') : photo.uri,
   });
 
-  Object.keys(body).forEach((key) => {
-    data.append(key, body[key]);
-  });
   return data;
 };
 
 function AddPhoto() {
   const [photo, setPhoto] = React.useState(null);
+  const [displayUrl, setDisplayUrl] = React.useState("");
 
   const handleChoosePhoto = async() => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -29,18 +29,55 @@ function AddPhoto() {
       allowsEditing: true,
       quality: 1,
     });
-    console.log(result);
     setPhoto(result);
   };
 
+  const cloudinaryUpload = (photo) => {
+    const data = new FormData()
+    data.append('file', photo)
+    data.append('upload_preset', 'imageuploads')
+    data.append("cloud_name", "dmunxqdae")
+    fetch("https://api.cloudinary.com/v1_1/dmunxqdae/upload", {
+      method: "POST",
+      body: data
+    }).then(res => res.json()).
+      then(data => {
+        console.log(data);
+        setDisplayUrl(data.secure_url);
+      }).catch(err => {
+        Alert.alert("An Error Occured While Uploading")
+      })
+  }
+
   const handleUploadPhoto = () => {
-    axios.post(SERVER_URL, { photo })
-          .then((response) => {
-            console.log(response);
-          })
-      .catch((error) => {
-        console.log('error', error);
-      });
+    const uri = photo.uri;
+    const type = photo.type;
+    let name = photo.fileName;
+    if(typeof photo.fileName === "undefined"){
+        var getFilename = uri.split("/");
+        name = getFilename[getFilename.length - 1];
+    }
+    const source = {
+      uri,
+      type,
+      name,
+    }
+    cloudinaryUpload(source);
+    axios.post(SERVER_URL, { displayUrl })
+    .then((response) => {
+      console.log(response);
+    })
+    // fetch(`${LOCALHOST_URL}/api/upload`, {
+    //   method: 'POST',
+    //   body: createFormData(photo),
+    // })
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     console.log('response', response);
+    //   })
+    //   .catch((error) => {
+    //     console.log('error', error);
+    //   });
   };
 
   return (
@@ -51,7 +88,7 @@ function AddPhoto() {
             source={{ uri: photo.uri }}
             style={{ width: 300, height: 300 }}
           />
-          <Button title="Upload Photo" onPress={handleUploadPhoto} />
+          <Button title="Upload Photo" onPress={handleUploadPhoto} encType='multipart/form-data'/>
         </>
       )}
       <Button title="Choose Photo" onPress={handleChoosePhoto} />
--- server.js
+++ server.js
@@ -2,29 +2,37 @@
 
 const express = require('express');
 const multer = require('multer');
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, './images');
+    callback(null, './uploads/');
   },
   filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+    let filename = Date.now() + '.png';
+    req.body.file = filename;
+    callback(null, filename);
   },
 });
 
-const upload = multer({ limits: { fieldSize: 25 * 1024 * 1024 }, storage });
+const upload = multer({ limits: { fieldSize: 10 * 1024 * 1024 }, storage: storage });
 
 app.get('/', (req, res) => {
   res.status(200).send('You can post to /api/upload.');
 });
 
-app.post('/api/upload', upload.array('photo', 3), (req, res) => {
-  console.log('file', req.files);
-  console.log('body', req.body);
+app.post('/api/upload', upload.single('photo'), (req, res) => {
+  const file = req.file;
+  if (!file) {
+    console.log('fuck me');
+  }
+  console.log(req.body);
+  console.log(req.body.file);
+  // console.log('file', req.files);
+  // console.log('body', req.body);
   res.status(200).json({
     message: 'success!',
   });
@@ -32,6 +40,6 @@ app.post('/api/upload', upload.array('photo', 3), (req, res) => {
 
 app.listen(3000, () => {
   console.log(
-    `server is running at http://10.0.0.84:${3000}`
+    'server is running at http://10.0.0.84:3000'
   );
 });
